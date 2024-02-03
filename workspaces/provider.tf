3fcf2464cd656036bf648152a39023c96dfb7516
saidevops32 <devopsengsai@gmail.com>
2024-02-03 21:06:56 +0530

    terraform-multienvironment

---
+++ workspaces/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.35.0"
+    }
+  }
+    backend "s3" {
+    bucket = "saidev-remote-state"
+    key    = "foreach"
+    region = "us-east-1"
+    dynamodb_table = "saidev-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
